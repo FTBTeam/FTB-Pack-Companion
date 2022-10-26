056f3b8723601a040b08ad0b3fc0cbbec8f75dd1
Michael Hillcox <michaelhillcox@gmail.com>
2022-10-26 13:08:50 +0100

    feat: release prep

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [2.0.0]
+
+### Added
+
+- Released the mod for 1.19.2
--- common/src/main/java/dev/ftb/packcompanion/CommandRegistry.java
+++ common/src/main/java/dev/ftb/packcompanion/CommandRegistry.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import dev.ftb.packcompanion.commands.CommandEntry;
 import dev.ftb.packcompanion.commands.LootTableGeneratorCommand;
+import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 
@@ -14,13 +15,13 @@ public class CommandRegistry {
         new LootTableGeneratorCommand()
     );
 
-    public static void setup(CommandDispatcher<CommandSourceStack> dispatcher, Commands.CommandSelection selection) {
-        LiteralArgumentBuilder<CommandSourceStack> companionRootCommand = Commands.literal(PackCompanion.MOD_ID);
+    public static void setup(CommandDispatcher<CommandSourceStack> commandDispatcher, CommandBuildContext commandBuildContext, Commands.CommandSelection commandSelection) {
+//        LiteralArgumentBuilder<CommandSourceStack> companionRootCommand = Commands.literal(PackCompanion.MOD_ID);
 
 //        for (CommandEntry command : COMMANDS) {
 //            companionRootCommand.then(command.register());
 //        }
 
-        dispatcher.register(companionRootCommand);
+//        commandDispatcher.register(companionRootCommand);
     }
 }
