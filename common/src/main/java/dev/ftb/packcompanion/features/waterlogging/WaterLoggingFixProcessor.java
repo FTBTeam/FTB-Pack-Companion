3219081aed4643a36f8dccdc0d94cdefdc6bf2ff
Des Herriott <des.herriott@gmail.com>
2023-09-01 08:14:54 +0100

    fix: make waterlogging processor work with ServerLevel too
    This way, it can also be used with `/place jigsaw` commands
    
    Also adds validation to ensure chunk is loaded (can be an issue
    with huge structures during worldgen)

--- common/src/main/java/dev/ftb/packcompanion/features/waterlogging/WaterLoggingFixProcessor.java
+++ common/src/main/java/dev/ftb/packcompanion/features/waterlogging/WaterLoggingFixProcessor.java
@@ -3,9 +3,10 @@ package dev.ftb.packcompanion.features.waterlogging;
 import com.mojang.serialization.Codec;
 import dev.ftb.packcompanion.registry.StructureProcessorRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.WorldGenRegion;
-import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.LevelWriter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
@@ -15,6 +16,8 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemp
 import net.minecraft.world.level.material.Fluids;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
+
 public class WaterLoggingFixProcessor extends StructureProcessor {
     private static final WaterLoggingFixProcessor INSTANCE = new WaterLoggingFixProcessor();
     public static final Codec<WaterLoggingFixProcessor> CODEC = Codec.unit(() -> INSTANCE);
@@ -22,19 +25,31 @@ public class WaterLoggingFixProcessor extends StructureProcessor {
     @Nullable
     @Override
     public StructureTemplate.StructureBlockInfo processBlock(LevelReader levelReader, BlockPos blockPos, BlockPos blockPos2, StructureTemplate.StructureBlockInfo original, StructureTemplate.StructureBlockInfo after, StructurePlaceSettings structurePlaceSettings) {
-        if (after.state.isAir() || !(levelReader instanceof WorldGenRegion worldGenRegion)) {
-            return after;
-        }
+        // NOTE: process() method is a Forge extension. If we ever port this to Fabric, use processBlock() instead
 
-        // Is the block meant to be a fluid? No then lets make sure the world doesn't already have a fluid there
-        if (after.state.getFluidState().isEmpty()) {
-            // Is the block water? No, is the current block in the world water? Yes, Cool, remove it before placement
-            if (after.state.getBlock() != Blocks.WATER && levelReader.getBlockState(after.pos).getFluidState().is(Fluids.WATER)) {
-                worldGenRegion.setBlock(after.pos, after.state, Block.UPDATE_ALL);
-            }
+        if (!after.state.isAir()) {
+            getWriter(levelReader).ifPresent(writer -> {
+                // Is the block meant to be a fluid? No then lets make sure the world doesn't already have a fluid there
+                if (after.state.getFluidState().isEmpty()) {
+                    // Is the block water? No, is the current block in the world water? Yes, Cool, remove it before placement
+                    if (after.state.getBlock() != Blocks.WATER && levelReader.hasChunkAt(after.pos) && levelReader.getBlockState(after.pos).getFluidState().is(Fluids.WATER)) {
+                        writer.setBlock(after.pos, after.state, Block.UPDATE_ALL);
+                    }
+                }
+            });
         }
 
         return after;
+    }
+
+    private Optional<LevelWriter> getWriter(LevelReader reader) {
+        if (reader instanceof WorldGenRegion wg) {
+            return Optional.of(wg);
+        } else if (reader instanceof ServerLevel sl) {
+            return Optional.of(sl);
+        } else {
+            return Optional.empty();
+        }
     }
 
     @Override
