b185bd01693d69434231b197ddc6f34d4dd9ed64
Des Herriott <des.herriott@gmail.com>
2023-09-07 09:19:26 +0100

    feat: mixin to increase max jigsaw size from 128 to 256

--- common/src/main/java/dev/ftb/packcompanion/config/PCCommonConfig.java
+++ common/src/main/java/dev/ftb/packcompanion/config/PCCommonConfig.java
@@ -11,6 +11,9 @@ public interface PCCommonConfig {
 
     BooleanValue ALLOW_BEDS_IN_THE_NETHER = CONFIG.getBoolean("allow_beds_in_the_nether", false)
             .comment("When enabled, players will be able to use beds in the nether.");
+    BooleanValue EXTENDED_JIGSAW_RANGE = CONFIG.getBoolean("extended_jigsaw_range", true)
+            .comment("When enabled, expand jigsaw feature size limit from 128 to 256.");
+
     static void load() {
         ConfigUtil.loadDefaulted(CONFIG, PackCompanionExpectPlatform.getConfigDirectory(), PackCompanion.MOD_ID);
     }
---
+++ common/src/main/java/dev/ftb/packcompanion/mixin/features/jigsaw/JigsawPlacementMixin.java
@@ -0,0 +1,15 @@
+package dev.ftb.packcompanion.mixin.features.jigsaw;
+
+import dev.ftb.packcompanion.config.PCCommonConfig;
+import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(JigsawPlacement.class)
+public class JigsawPlacementMixin {
+    @ModifyVariable(method = "addPieces(Lnet/minecraft/world/level/levelgen/structure/Structure$GenerationContext;Lnet/minecraft/core/Holder;Ljava/util/Optional;ILnet/minecraft/core/BlockPos;ZLjava/util/Optional;I)Ljava/util/Optional;", at = @At("HEAD"), ordinal = 1, argsOnly = true)
+    private static int modifyRange(int range) {
+        return PCCommonConfig.EXTENDED_JIGSAW_RANGE.get() ? 256 : range;
+    }
+}
