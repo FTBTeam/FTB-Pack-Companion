13c238f5a7aaa3b0d7f989109523b9385b43d121
Michael Hillcox <michaelhillcox@gmail.com>
2023-09-14 21:27:20 +0100

    feat: added optional spawner break punishment

--- common/src/main/java/dev/ftb/packcompanion/config/PCServerConfig.java
+++ common/src/main/java/dev/ftb/packcompanion/config/PCServerConfig.java
@@ -20,6 +20,8 @@ public interface PCServerConfig {
     StringListValue SPAWNERS_USE_RANDOM_ENTITY = SPAWNERS.getStringList("random_entity", new ArrayList<>())
             .comment("A list of entity types that will be used to replace broken spawners. Set to an empty list to disable.");
 
+    BooleanValue PUNISH_BREAKING_SPAWNER = SPAWNERS.getBoolean("punish_for_breaking_spawners", false);
+
     DoubleValue MODIFY_MOB_BASE_HEALTH = CONFIG.getDouble("modify_mob_base_health", 0D, 0D, 1000D)
             .comment("If non-zero, set the base health of all mobs to be multiplied by this value. Set to 0 to disable.");
 
