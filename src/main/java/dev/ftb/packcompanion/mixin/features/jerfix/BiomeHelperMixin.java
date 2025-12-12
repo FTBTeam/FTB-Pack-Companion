cab204d6581a48c97a00b36f77b1382833048b6e
Saereth <paulanthonydevs@gmail.com>
2025-12-12 09:28:24 -0800

    Fixes biome access during JER initialization
    Changes the BiomeHelperMixin to inject into JER's biome access methods instead of overwriting them

--- src/main/java/dev/ftb/packcompanion/mixin/features/jerfix/BiomeHelperMixin.java
+++ src/main/java/dev/ftb/packcompanion/mixin/features/jerfix/BiomeHelperMixin.java
@@ -1,16 +1,16 @@
 package dev.ftb.packcompanion.mixin.features.jerfix;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.registries.VanillaRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,82 +27,47 @@ import java.util.Optional;
 @Mixin(targets = "jeresources.api.util.BiomeHelper", remap = false)
 public class BiomeHelperMixin {
 
-    /**
-     * Fallback to vanilla registry if any aren't available
-     */
-    private static List<Biome> fallbackGetAllBiomes() {
-        List<Biome> biomes = new ArrayList<>();
-        VanillaRegistries.createLookup().lookupOrThrow(Registries.BIOME)
-                .listElements().map(Holder.Reference::value).forEach(biomes::add);
-        return biomes;
+    @Inject(method = "getAllBiomes", at = @At("HEAD"), cancellable = true)
+    private static void getAllBiomes(CallbackInfoReturnable<List<Biome>> cir) {
+        Level level = Minecraft.getInstance().level;
+        if (level != null) {
+            Optional<Registry<Biome>> registry = level.registryAccess().registry(Registries.BIOME);
+            if (registry.isPresent()) {
+                List<Biome> biomes = new ArrayList<>();
+                registry.get().forEach(biomes::add);
+                cir.setReturnValue(biomes);
+            }
+        }
     }
 
-    private static Biome fallbackGetBiome(ResourceKey<Biome> key) {
-        return VanillaRegistries.createLookup().lookupOrThrow(Registries.BIOME)
-                .getOrThrow(key).value();
+    @Inject(method = "getBiome", at = @At("HEAD"), cancellable = true)
+    private static void getBiome(ResourceKey<Biome> key, CallbackInfoReturnable<Biome> cir) {
+        Level level = Minecraft.getInstance().level;
+        if (level != null) {
+            Optional<Registry<Biome>> registry = level.registryAccess().registry(Registries.BIOME);
+            if (registry.isPresent()) {
+                Biome biome = registry.get().get(key);
+                if (biome != null) {
+                    cir.setReturnValue(biome);
+                }
+            }
+        }
     }
 
-    private static List<Biome> fallbackGetBiomes(ResourceKey<Biome> category) {
-        List<Biome> biomes = new ArrayList<>();
-        VanillaRegistries.createLookup().lookupOrThrow(Registries.BIOME)
-                .listElements().forEach(biomeEntry -> {
+    @Inject(method = "getBiomes", at = @At("HEAD"), cancellable = true)
+    private static void getBiomes(ResourceKey<Biome> category, CallbackInfoReturnable<List<Biome>> cir) {
+        Level level = Minecraft.getInstance().level;
+        if (level != null) {
+            Optional<Registry<Biome>> registry = level.registryAccess().registry(Registries.BIOME);
+            if (registry.isPresent()) {
+                List<Biome> biomes = new ArrayList<>();
+                registry.get().asLookup().listElements().forEach(biomeEntry -> {
                     if (biomeEntry.key().equals(category)) {
                         biomes.add(biomeEntry.value());
                     }
                 });
-        return biomes;
-    }
-
-    @Overwrite
-    public static List<Biome> getAllBiomes() {
-        Level level = Minecraft.getInstance().level;
-        if (level == null) {
-            return fallbackGetAllBiomes();
-        }
-
-        Optional<Registry<Biome>> registry = level.registryAccess().registry(Registries.BIOME);
-        if (registry.isEmpty()) {
-            return fallbackGetAllBiomes();
-        }
-
-        List<Biome> biomes = new ArrayList<>();
-        registry.get().forEach(biomes::add);
-        return biomes;
-    }
-
-    @Overwrite
-    public static Biome getBiome(ResourceKey<Biome> key) {
-        Level level = Minecraft.getInstance().level;
-        if (level == null) {
-            return fallbackGetBiome(key);
-        }
-
-        Optional<Registry<Biome>> registry = level.registryAccess().registry(Registries.BIOME);
-        if (registry.isEmpty()) {
-            return fallbackGetBiome(key);
-        }
-
-        return registry.get().get(key);
-    }
-
-    @Overwrite
-    public static List<Biome> getBiomes(ResourceKey<Biome> category) {
-        Level level = Minecraft.getInstance().level;
-        if (level == null) {
-            return fallbackGetBiomes(category);
-        }
-
-        Optional<Registry<Biome>> registry = level.registryAccess().registry(Registries.BIOME);
-        if (registry.isEmpty()) {
-            return fallbackGetBiomes(category);
-        }
-
-        List<Biome> biomes = new ArrayList<>();
-        registry.get().asLookup().listElements().forEach(biomeEntry -> {
-            if (biomeEntry.key().equals(category)) {
-                biomes.add(biomeEntry.value());
+                cir.setReturnValue(biomes);
             }
-        });
-        return biomes;
+        }
     }
 }
